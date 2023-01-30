180c9a69041440faeeea31d40ccd95ae2ca99a1b
alllq <34611623+aalllq@users.noreply.github.com>
2023-01-30 23:07:59 +0300

    add  skeleton gui filles

---
+++ start_gui.py
@@ -0,0 +1,58 @@
+'''start file, gui app, use pyside6'''
+
+import sys,os
+from PySide6 import QtWidgets
+import lib_api_pyside 
+from modules import *
+import logging
+# simple pyside6app
+class lib_api_pyside_App(QtWidgets.QMainWindow, lib_api_pyside.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)  
+        self.btn_all_device_info.clicked.connect(self.btn_all_info)
+        self.ALL_textEdit.append("ALL")
+        self.logTextBox.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+        #logging.getLogger().addHandler(logTextBox)
+        #logging.g
+   
+    def btn_all_info(self):
+        data_writter("all_device","to_excel")
+    #logging to self.All_plainTextEdit
+    
+    
+    def emit(self, record):
+        msg0 = self.format(record)
+        self.PVELogs.appendPlainText(msg0)
+
+        msg1 = self.format(record)
+        self.PVPLogs.appendPlainText(msg1)
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+''''В этом классе мы будем взаимодействовать с элементами интерфейса,
+добавлять соединения и всё остальное, что нам потребуется. Но для начала
+нам нужно инициализировать класс при запуске кода. С этим мы разберёмся в функции main():'''      
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)  
+    window = lib_api_pyside_App()  #
+    window.show()  
+    app.exec()       
+
+if __name__ == '__main__':  # Если мы запускаем файл напрямую, а не импортируем
+    main()  # то запускаем функцию main()
+
+
+
