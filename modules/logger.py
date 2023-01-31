fa4ad408a17624496918d0fafe443d3c9a91d09b
alllq <34611623+aalllq@users.noreply.github.com>
2023-01-31 11:46:39 +0300

    remake logger.py

--- modules/logger.py
+++ modules/logger.py
@@ -1,21 +1,27 @@
-import asyncio
-from aiologger import Logger
+import logging,arrow
+from logging.handlers import RotatingFileHandler
+start_time=arrow.now().format("YY-MM-DD")
 
-
-async def get_app_logger(logger_name):
-    logger = Logger.with_default_handlers(name=logger_name)
-    logger.setLevel(logging.INFO)
+def get_logger(name):
+    """получает __name__  возвращает logger"""
+    log_file_name=f"log/{start_time}.log"
+    logger= logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    #file
+    file_handler = logging.FileHandler(log_file_name)
+    file_handler.setLevel(logging.DEBUG)
+    #stdout
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(logging.DEBUG)
-    file_handler = logging.FileHandler("x.log")
-    file_handler.setLevel(logging.WARNING)
-    #await logger.shutdown()
-    logger.addHandler(stream_handler)
+    #formatter
+    _log_format = f"%(asctime)s - [%(levelname)s] - %(name)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s"
+    formatter = logging.Formatter(_log_format)
+    file_handler.setFormatter(formatter)
+    stream_handler.setFormatter(formatter)
+    #add handler
     logger.addHandler(file_handler)
-    return(logger)
-#loop = asyncio.get_event_loop()
-#loop.run_until_complete(get_app_logger())
-#loop.close()
-#loop.run_forever()
-
-
+    logger.addHandler(stream_handler)
+    #rotate
+    rotate_handler = RotatingFileHandler(log_file_name,maxBytes=(5*1024)*1024,backupCount=10)
+    logger.addHandler(rotate_handler)
+    return logger
